f9fb92ac6e8e8a68d357cdf605472814cf0a40e0
artistmait <maitreyeepuranik@gmail.com>
2026-04-22 20:48:34 +0530

    refactor : shifted to resend emails for email service

--- nucleus_backend/config/email.js
+++ nucleus_backend/config/email.js
@@ -1,11 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 import nodemailer from "nodemailer";
+import {Resend} from "resend";
 
-export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // app password
-  },
-});
+export const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS, // app password
+//   },
+// });
